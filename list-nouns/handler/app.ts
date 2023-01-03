731c4e429111456653d7af173e646ea745c59a27
Andrew Monteiro <andrew.agoravai@gmail.com>
2023-01-02 22:46:36 -0300

    feat: add aws-sdk and use real data from DynamoDB in ListNouns function

--- list-nouns/handler/app.ts
+++ list-nouns/handler/app.ts
@@ -1,14 +1,23 @@
-import {APIGatewayProxyEvent, APIGatewayProxyResult} from 'aws-lambda'
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import { DynamoDB } from 'aws-sdk'
 
-const fakeNouns = [
-  {id: 'fakeId1', noun: 'Apfel', gender: 'masculine', article: 'der'},
-  {id: 'fakeId2', noun: 'Sonne', gender: 'feminine', article: 'die'},
-  {id: 'fakeId3', noun: 'Mädchen', gender: 'neuter', article: 'das'},
-]
+const docClient = new DynamoDB.DocumentClient()
+const params = {
+  TableName: 'WORTERBUCH'
+}
 
 export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(fakeNouns)
+  try {
+    const data = await docClient.scan(params).promise()
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data)
+    }
+  } catch (err) {
+    console.error(`An error was thrown while reading the data: ${err}`)
+    return {
+      statusCode: err.status ? err.status : 500,
+      body: JSON.stringify({ message: err.message ? err.message : 'An unknown error was thrown. Please get in touch with us!' })
+    }
   }
 }
