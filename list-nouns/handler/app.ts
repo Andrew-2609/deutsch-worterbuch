7725f1abdec1063acd7023dd430b3c9749ec7279
Andrew Monteiro <andrew.agoravai@gmail.com>
2023-01-03 00:26:05 -0300

    feat: add possibility to pass the gender as a query parameter in ListNounsFunction

--- list-nouns/handler/app.ts
+++ list-nouns/handler/app.ts
@@ -2,11 +2,14 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import { DynamoDB } from 'aws-sdk'
 
 const docClient = new DynamoDB.DocumentClient()
-const params = {
-  TableName: 'WORTERBUCH'
-}
 
 export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const params: DynamoDB.DocumentClient.QueryInput = { TableName: 'WORTERBUCH', ProjectionExpression: 'noun, article, gender' }
+  const { queryStringParameters } = event
+  
+  if (queryStringParameters?.gender)
+    setGenderFromQueryStringParameters(params, queryStringParameters.gender)
+
   try {
     const { Items } = await docClient.scan(params).promise()
     return {
@@ -19,5 +22,15 @@ export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGat
       statusCode: err.status ? err.status : 500,
       body: JSON.stringify({ message: err.message ? err.message : 'An unknown error was thrown. Please get in touch with us!' })
     }
+  }
+}
+
+const setGenderFromQueryStringParameters = (params: DynamoDB.DocumentClient.QueryInput, gender: string) => {
+  params.FilterExpression = '#gender = :gender'
+  params.ExpressionAttributeNames = {
+    '#gender': 'gender',
+  }
+  params.ExpressionAttributeValues = {
+    ':gender': gender
   }
 }
