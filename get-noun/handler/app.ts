ab6f26cd1ed16f011683b2705b82cf8cc437327c
Andrew Monteiro <andrew.agoravai@gmail.com>
2023-01-10 18:54:48 -0300

    feat: add basic error handling to app

--- get-noun/handler/app.ts
+++ get-noun/handler/app.ts
@@ -6,18 +6,26 @@ export const docClient = new DynamoDB.DocumentClient()
 const params: DynamoDB.DocumentClient.QueryInput = { TableName: 'WORTERBUCH' }
 
 export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const { noun } = event.pathParameters
-  const { Item } = await docClient.get({ ...params, Key: { noun: noun } }).promise()
+  try {
+    const { noun } = event.pathParameters
+    const { Item } = await docClient.get({ ...params, Key: { noun: noun } }).promise()
 
-  if (!Item) {
-    return {
-      statusCode: 404,
-      body: JSON.stringify({ message: 'Noun not found!' })
+    if (!Item) {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({ message: 'Noun not found!' })
+      }
     }
-  }
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify(Item)
+    return {
+      statusCode: 200,
+      body: JSON.stringify(Item)
+    }
+  } catch (err) {
+    console.error(`An error was thrown while reading the data: ${err}`)
+    return {
+      statusCode: err.status ? err.status : 500,
+      body: JSON.stringify({ message: err.message ? err.message : 'An unknown error was thrown. Please get in touch with us!' })
+    }
   }
 }
